d6d65f92c495f0ba67eb29ed5bfa47febe8173ea
mudhar <mudharrizki@gmail.com>
2019-11-03 18:43:56 +0700

    group cash back dan sum total keseluruhan

--- sale_cashback/models/stock_picking.py
+++ sale_cashback/models/stock_picking.py
@@ -7,8 +7,17 @@ class StockPicking(models.Model):
     @api.model
     def _get_invoice_vals(self, key, inv_type, journal_id, move):
         result = super(StockPicking, self)._get_invoice_vals(key, inv_type, journal_id, move)
-        if move.group_id:
-            sale_id = self.env['sale.order'].search([('procurement_group_id', '=', move.group_id.id)])
-            result.update({'cash_back': sum(sale_id.mapped('cash_back'))})
+        context = self.env.context
+        picking_ids = self.env['stock.picking'].browse(context['active_ids'])
+        group_ids = picking_ids.mapped('group_id')
+
+        partner, currency_id, company_id, user_id = key
+        if move.group_id and partner:
+            sale_ids = self.env['sale.order'].search(
+                [('procurement_group_id', 'in', group_ids.ids),
+                 ('partner_id', '=', partner.id),
+                 ('company_id', '=', company_id)])
+            result.update({'cash_back': sum(sale_ids.mapped('cash_back'))})
+
         return result
 
--- sale_cashback/wizards/account_invoice_wizard.py
+++ sale_cashback/wizards/account_invoice_wizard.py
@@ -11,10 +11,15 @@ class AccountInvoiceWizard(models.TransientModel):
         active_ids = self._context.get('active_ids')
         invoice_ids = self.env['account.invoice']
         for invoice in invoice_ids.browse(active_ids):
-            move_id = invoice.invoice_line.mapped('move_id')
-            assert len(move_id) == 1
-            sale_id = self.env['sale.order'].search([('procurement_group_id', '=', move_id.group_id.id)])
-            invoice.update({'cash_back': sum(sale_id.mapped('cash_back'))})
+            move_ids = invoice.invoice_line.mapped('move_id')
+            group_ids = move_ids.mapped('group_id')
+            sale_ids = self.env['sale.order'].search(
+                [('procurement_group_id', 'in', group_ids.ids),
+                 ('partner_id', '=', invoice.partner_id.id),
+                 ('company_id', '=', invoice.company_id.id),
+                 ('state', 'not in', ('cancel', 'draft'))])
+
+            invoice.update({'cash_back': sum(sale_ids.mapped('cash_back'))})
         return {'type': 'ir.actions.act_window_close'}
 
 
