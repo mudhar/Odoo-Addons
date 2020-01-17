3669e8fbea1f620de1fe13c43e1d5b7d00ede43d
mudhar <mudharrizki@gmail.com>
2020-01-18 04:28:06 +0700

    Set Flag Copy False

--- textile_assembly/models/product_template.py
+++ textile_assembly/models/product_template.py
@@ -5,8 +5,8 @@ from odoo.exceptions import ValidationError
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    template_code = fields.Char(string="Code")
-    code_pola = fields.Char(string="Code Pola")
+    template_code = fields.Char(string="Assembly Code", copy=False)
+    code_pola = fields.Char(string="Code Pola", copy=False)
     is_roll = fields.Boolean(string="Is Roll?")
 
     @api.constrains('is_roll')
@@ -17,3 +17,12 @@ class ProductTemplate(models.Model):
                                         "Pada Tab Inventory/Tracking\n"
                                         "Anda Wajib Mimilih Opsi BY LOTS"))
         return True
+
+    # @api.depends('template_code')
+    # def _compute_template_code(self):
+    #     for res in self:
+    #         template_code = res.template_code.strip().split(sep=" ")
+    #         code_join = ''.join(code.lower() for code in template_code)
+    #         if code_join:
+    #             res.assembly_code |= code_join
+
