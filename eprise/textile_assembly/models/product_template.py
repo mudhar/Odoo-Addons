a423112f8d7d6273ecf77f87d74c238ebfa2601f
mudhar <mudharrizki@gmail.com>
2020-08-18 15:40:13 +0700

    remove directory eprise

--- eprise/textile_assembly/models/product_template.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    template_code = fields.Char(string="Code", required=True)
--- eprise/textile_assembly/models/res_company.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class ResCompany(models.Model):
-    _inherit = 'res.company'
-
-    location_id = fields.Many2one(comodel_name="stock.location", string="Location", domain=[('usage', '=', 'internal')])
