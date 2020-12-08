7fe4f6f18bc2f64a5ccd6abacf164a2173f10732
rizki <mudharrizki@gmail.com>
2020-12-08 16:25:30 +0700

    fix positon argument

--- subsequence_date_range/models/assembly_plan.py
+++ subsequence_date_range/models/assembly_plan.py
@@ -8,21 +8,20 @@ class AssemblyPlan(models.Model):
     @api.model
     def create(self, values):
         sequence_id = self.env['ir.sequence'].search([('code', '=', 'assembly.plan')])
-        sequence_object = self.env['ir.sequence']
         if values.get('assembly_id') and sequence_id.use_date_range:
             partner_id = self.env['res.partner'].browse(values.get('partner_id'))
             date_to = fields.Date.from_string(values.get('date_planned_finished'))
             date_to_string = date_to.strftime('%Y-%m-%d')
             # 2020-01-01
-            date_range = sequence_object._find_date_range_seq(sequence_id, date_to_string)
-            dt_from, dt_to = sequence_object._format_date_range_seq(date_to)
+            date_range = self.env['ir.sequence']._find_date_range_seq(sequence_id, date_to_string)
+            dt_from, dt_to = self.env['ir.sequence']._format_date_range_seq(date_to)
             if not date_range:
-                seq_date = sequence_object.act_create_date_range_seq(dt_from, dt_to, sequence_id)
-                seq_prefix = sequence_object._create_sequence_prefix(sequence_id, seq_date)
+                seq_date = self.env['ir.sequence'].act_create_date_range_seq(dt_from, dt_to, sequence_id)
+                seq_prefix = self.env['ir.sequence']._create_sequence_prefix(sequence_id, seq_date)
 
                 values['name'] = self._create_assembly_plan_prefix(partner_id, seq_prefix)
             else:
-                seq_prefix = sequence_object._create_sequence_prefix(sequence_id, date_range)
+                seq_prefix = self.env['ir.sequence']._create_sequence_prefix(sequence_id, date_range)
                 values['name'] = self._create_assembly_plan_prefix(partner_id, seq_prefix)
 
         return super(AssemblyPlan, self).create(values)
--- subsequence_date_range/models/purchase_order.py
+++ subsequence_date_range/models/purchase_order.py
@@ -8,38 +8,37 @@ class PurchaseOrder(models.Model):
 
     @api.model
     def create(self, vals):
-        sequence_object = self.env['ir.sequence']
         if vals.get('product_select_type', 'materials') == 'materials':
-            sequence_id = sequence_object.search([('code', '=', 'purchase.order.materials')])
+            sequence_id = self.env['ir.sequence'].search([('code', '=', 'purchase.order.materials')])
             if sequence_id.use_date_range and vals.get('date_order'):
                 # 2020-01-10
                 date_to = fields.Date.from_string(vals.get('date_order'))
-                dt_from, dt_to = sequence_object._format_date_range_seq(date_to)
+                dt_from, dt_to = self.env['ir.sequence']._format_date_range_seq(date_to)
                 date_to_string = date_to.strftime('%Y-%m-%d')
-                date_range = sequence_object._find_date_range_seq(date_to_string, sequence_id)
+                date_range = self.env['ir.sequence']._find_date_range_seq(sequence_id, date_to_string)
                 if not date_range:
-                    seq_date = sequence_object.act_create_date_range_seq(dt_from, dt_to, sequence_id)
+                    seq_date = self.env['ir.sequence'].act_create_date_range_seq(dt_from, dt_to, sequence_id)
                     # seq_prefix
-                    vals['name'] = sequence_object._create_sequence_prefix(sequence_id, seq_date)
+                    vals['name'] = self.env['ir.sequence']._create_sequence_prefix(sequence_id, seq_date)
                 else:
                     # seq_prefix
-                    vals['name'] = sequence_object._create_sequence_prefix(sequence_id, date_range)
+                    vals['name'] = self.env['ir.sequence']._create_sequence_prefix(sequence_id, date_range)
 
         if vals.get('product_select_type', 'goods') == 'goods':
-            sequence_id = sequence_object.search([('code', '=', 'purchase.order.goods')])
+            sequence_id = self.env['ir.sequence'].search([('code', '=', 'purchase.order.goods')])
             if sequence_id.use_date_range and vals.get('date_order'):
                 # 2020-01-10
                 date_to = fields.Date.from_string(vals.get('date_order'))
-                dt_from, dt_to = sequence_object._format_date_range_seq(date_to)
+                dt_from, dt_to = self.env['ir.sequence']._format_date_range_seq(date_to)
                 date_to_string = date_to.strftime('%Y-%m-%d')
-                date_range = sequence_object._find_date_range_seq(date_to_string, sequence_id)
+                date_range = self.env['ir.sequence']._find_date_range_seq(sequence_id, date_to_string)
                 if not date_range:
-                    seq_date = sequence_object.act_create_date_range_seq(dt_from, dt_to, sequence_id)
+                    seq_date = self.env['ir.sequence'].act_create_date_range_seq(dt_from, dt_to, sequence_id)
                     # seq_prefix
-                    vals['name'] = sequence_object._create_sequence_prefix(sequence_id, seq_date)
+                    vals['name'] = self.env['ir.sequence']._create_sequence_prefix(sequence_id, seq_date)
                 else:
                     # seq_prefix
-                    vals['name'] = sequence_object._create_sequence_prefix(sequence_id, date_range)
+                    vals['name'] = self.env['ir.sequence']._create_sequence_prefix(sequence_id, date_range)
 
         return super(PurchaseOrder, self).create(vals)
 
