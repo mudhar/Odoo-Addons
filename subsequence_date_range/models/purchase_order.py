945a33c4e03901d49f7cb4d346da089a6fe9f355
rizki <mudharrizki@gmail.com>
2020-12-08 15:50:34 +0700

    refactor fungsi

--- subsequence_date_range/models/purchase_order.py
+++ subsequence_date_range/models/purchase_order.py
@@ -1,5 +1,5 @@
+# -*- coding: utf-8 -*-
 import calendar
-from datetime import datetime
 from odoo import fields, models, api
 
 
@@ -14,30 +14,37 @@ class PurchaseOrder(models.Model):
             if sequence_id.use_date_range and vals.get('date_order'):
                 # 2020-01-10
                 date_to = fields.Date.from_string(vals.get('date_order'))
-                year = date_to.year
-                month = date_to.month
-                days = calendar.monthrange(year, month)
-
-                # date_range = sequence_id.date_range_ids.filtered(
-                #     lambda dt: dt.date_from <= date_to.strftime('%Y-%m-%d') <= dt.date_to)
+                dt_from, dt_to = self._format_date_range_seq(date_to)
                 date_to_string = date_to.strftime('%Y-%m-%d')
-                date_range = self.env['ir.sequence.date_range'].search(
-                    [('sequence_id', '=', sequence_id.id), ('date_from', '<=', date_to_string),
-                     ('date_to', '>=', date_to_string)], order='date_to desc', limit=1)
+                date_range = self._find_date_range_seq(date_to_string, sequence_id)
                 if not date_range:
-                    dt_from = '{}-{}-01'.format(year, month)
-                    dt_to = '{}-{}-{}'.format(year, month, days[1])
-                    seq_date = self.env['ir.sequence.date_range'].create({
-                        'sequence_id': sequence_id.id,
-                        'date_from': dt_from,
-                        'date_to': dt_to
-                    })
-                    seq_prefix = self.env['assembly.plan']._create_sequence_prefix(sequence_id, seq_date)
-                    vals['name'] = seq_prefix
+                    seq_date = self._create_date_range_seq(dt_from, dt_to, sequence_id)
+                    # seq_prefix
+                    vals['name'] = self.env['assembly.plan']._create_sequence_prefix(sequence_id, seq_date)
                 else:
-                    seq_prefix = self.env['assembly.plan']._create_sequence_prefix(sequence_id, date_range)
-                    vals['name'] = seq_prefix
+                    # seq_prefix
+                    vals['name'] = self.env['assembly.plan']._create_sequence_prefix(sequence_id, date_range)
 
         return super(PurchaseOrder, self).create(vals)
+
+    def _format_date_range_seq(self, date_to):
+        year = date_to.year
+        month = date_to.month
+        days = calendar.monthrange(year, month)
+        date_from = '{}-{}-01'.format(year, month)
+        date_to = '{}-{}-{}'.format(year, month, days[1])
+        return date_from, date_to
+
+    def _create_date_range_seq(self, dt_from, dt_to, sequence_id):
+        return self.env['ir.sequence.date_range'].create({
+            'sequence_id': sequence_id.id,
+            'date_from': dt_from,
+            'date_to': dt_to
+        })
+
+    def _find_date_range_seq(self, date_to_string, sequence_id):
+        return self.env['ir.sequence.date_range'].search(
+            [('sequence_id', '=', sequence_id.id), ('date_from', '<=', date_to_string),
+             ('date_to', '>=', date_to_string)], order='date_to desc', limit=1)
 
 
